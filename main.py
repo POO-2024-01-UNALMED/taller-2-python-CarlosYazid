c5759f42d4fabfba8a8b136ae71d9893c5750743
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2024-02-15 22:53:22 -0500

    Realizo el primer intento

---
+++ main.py
@@ -0,0 +1,83 @@
+"""
+  Taller 2 Python
+
+  Realizado el 15 de febrero del 2024
+  Desarrollador: Carlos Yazid Padilla
+  Topico: Clases y objetos
+"""
+class Asiento:
+
+  #  Constructor
+  def __init__(self, color: str, precio : int, registro : int) -> None:
+    self.color = color.lower()
+    self.precio = precio
+    self.registro = registro
+
+  #  Metodos de instancia
+  def cambiaColor(self, color: str) -> None:
+    match color:
+      case "amarillo":
+        self.color = "amarillo"
+      case "rojo":
+        self.color = "rojo"
+      case "negro":
+        self.color = "negro"
+      case "blanco":
+        self.color = "blanco"
+      case "verde":
+        self.color = "verde"
+  
+  pass
+
+class Motor:
+
+  #  Constructor
+  def __init__(self, numeroCilindros: int, tipo: str, registro : int) -> None:
+    self.numeroCilindros = numeroCilindros
+    self.tipo = tipo.lower()
+    self.registro = registro
+
+  #  Metodos de instancia
+  def cambiarRegistro(self, registro: int) -> None:
+    self.registro = registro
+
+  def asignarTipo(self, tipo: str) -> None:
+    if (tipo.lower() == "gasolina"):
+      self.tipo = "gasolina"
+    elif (tipo.lower() == "electrico"):
+      self.tipo = "electrico"
+  
+  pass
+
+class Auto:
+
+  #  Atributos de clase
+  cantidadCreados = 0
+
+  #  Constructor
+  def __init__(self, modelo : str, precio : int, asientos : list, marca : str, motor : Motor, registro : int) -> None:
+    self.modelo = modelo
+    self.precio = precio
+    self.asientos = asientos
+    self.marca = marca
+    self.motor = motor
+    self.registro = registro
+    Auto.cantidadCreados += 1
+
+  #  Metodos de instancia
+  def cantidadAsientos(self) -> int:
+    return len(self.asientos)
+
+  def verificarIntegridad(self) -> str:
+    if (self.registro == self.motor.registro):
+      for i in self.asientos:
+        if (i is None):
+            continue
+        else:
+          if (i.registro != self.registro):
+            return "Las piezas no son originales"
+      return "Auto Original"
+    return "Las piezas no son originales"
+  
+  pass
+#   Anti - copy: Creado por Carlos Yazid Padilla
